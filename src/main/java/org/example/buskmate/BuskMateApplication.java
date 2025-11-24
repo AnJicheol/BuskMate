327c616b8d4aa4c77cb2fc0285558eb9e0e2eaf3
AnJicheol <kany0511@gmail.com>
2025-11-24 09:12:19 +0900

    Initial commit

---
+++ src/main/java/org/example/buskmate/BuskMateApplication.java
@@ -0,0 +1,13 @@
+package org.example.buskmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuskMateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuskMateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/buskmate/BuskMateApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.buskmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuskMateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
