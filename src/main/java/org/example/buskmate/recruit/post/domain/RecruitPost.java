683339082e34b1cdee0f0807f530405988c2eeb0
k1myj <kimyujeong1204@gmail.com>
2025-11-25 13:53:53 +0900

    feat: recruitpost entity 생성

--- src/main/java/org/example/buskmate/recruit/post/domain/RecruitPost.java
+++ src/main/java/org/example/buskmate/recruit/post/domain/RecruitPost.java
@@ -6,6 +6,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
+import org.example.buskmate.band.domain.Band;
 
 import java.time.LocalDateTime;
 /**
