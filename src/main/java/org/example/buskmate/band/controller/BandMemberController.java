22f792752ccbb194174f0fa44681784a5fd0e605
estherkim0109 <dptmej0501@naver.com>
2025-11-28 16:27:56 +0900

    feat : band member 전체 조회

---
+++ src/main/java/org/example/buskmate/band/controller/BandMemberController.java
@@ -0,0 +1,25 @@
+package org.example.buskmate.band.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.example.buskmate.band.dto.BandMemberListItemResponse;
+import org.example.buskmate.band.service.BandMemberService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/bands")
+public class BandMemberController {
+
+    private final BandMemberService bandMemberService;
+
+    @GetMapping("/{bandId}/members")
+    public ResponseEntity<List<BandMemberListItemResponse>> getMembers(
+            @PathVariable String bandId
+    ) {
+        List<BandMemberListItemResponse> members = bandMemberService.getMembers(bandId);
+        return ResponseEntity.ok(members);
+    }
+}
