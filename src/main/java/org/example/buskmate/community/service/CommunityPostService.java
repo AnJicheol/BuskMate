24b6aa9e8d4293d6e90c0a1e8b69fbb511f800f5
nornsong <ahg077126@gmail.com>
2025-12-02 17:25:26 +0900

    feat: 서비스 블록 추가

--- src/main/java/org/example/buskmate/community/service/CommunityPostService.java
+++ src/main/java/org/example/buskmate/community/service/CommunityPostService.java
@@ -1,6 +1,20 @@
 package org.example.buskmate.community.service;
 
+import org.example.buskmate.community.dto.crud.request.CreatePostRequest;
+import org.example.buskmate.community.dto.crud.request.DeletePostRequest;
+import org.example.buskmate.community.dto.crud.request.UpdatePostRequest;
+import org.example.buskmate.community.dto.crud.response.CreatePostResponse;
+import org.example.buskmate.community.dto.crud.response.DeletePostResponse;
+import org.example.buskmate.community.dto.crud.response.PostIdResponse;
+import org.example.buskmate.community.dto.crud.response.UpdatePostResponse;
+
 public interface CommunityPostService {
 
+    CreatePostResponse createPost(CreatePostRequest request);
 
+    PostIdResponse getPostById(String id);
+
+    UpdatePostResponse updatePost(UpdatePostRequest request);
+
+    DeletePostResponse deletePost(DeletePostRequest request);
 }
--- src/main/java/org/example/buskmate/community/service/CommunityPostServiceImpl.java
+++ src/main/java/org/example/buskmate/community/service/CommunityPostServiceImpl.java
@@ -1,4 +1,35 @@
 package org.example.buskmate.community.service;
 
-public class CommunityPostServiceImpl {
+import lombok.RequiredArgsConstructor;
+import org.example.buskmate.community.dto.crud.request.CreatePostRequest;
+import org.example.buskmate.community.dto.crud.request.DeletePostRequest;
+import org.example.buskmate.community.dto.crud.request.UpdatePostRequest;
+import org.example.buskmate.community.dto.crud.response.CreatePostResponse;
+import org.example.buskmate.community.dto.crud.response.DeletePostResponse;
+import org.example.buskmate.community.dto.crud.response.PostIdResponse;
+import org.example.buskmate.community.dto.crud.response.UpdatePostResponse;
+import org.example.buskmate.community.repository.CommunityPostRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CommunityPostServiceImpl implements CommunityPostService {
+
+    private final CommunityPostRepository communityPostRepo;
+
+    public CreatePostResponse createPost(CreatePostRequest request){
+        return null;
+    }
+
+    public PostIdResponse getPostById(String id){
+        return null;
+    }
+
+    public UpdatePostResponse updatePost(UpdatePostRequest request){
+        return null;
+    }
+
+    public DeletePostResponse deletePost(DeletePostRequest request){
+        return null;
+    }
 }
