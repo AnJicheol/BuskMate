180e116074545d6a1d8926e1877021b44499fe4b
AnJicheol <kany0511@gmail.com>
2025-12-03 13:27:29 +0900

    feat: member 도메인 개발

---
+++ src/main/java/org/example/buskmate/auth/domain/member.java
@@ -0,0 +1,41 @@
+package org.example.buskmate.auth.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Table(
+        name = "members"
+)
+@Getter
+@NoArgsConstructor
+public class member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_id", nullable = false, length = 26, unique = true)
+    private String userId;
+
+    @Column(name = "name", length = 50)
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "provider", nullable = false, length = 20)
+    private OAuthProvider provider;
+
+    @Column(name = "provider_user_id", nullable = false, length = 100)
+    private String providerUserId;
+
+    public member(String userId, String name, String email, OAuthProvider provider, String providerUserId) {
+        this.userId = userId;
+        this.name = name;
+        this.email = email;
+        this.provider = provider;
+        this.providerUserId = providerUserId;
+    }
+}
