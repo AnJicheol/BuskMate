36527863b2cafdd50bb0866b81b3fa2a4c40fc3d
AnJicheol <kany0511@gmail.com>
2025-12-09 09:44:44 +0900

    .

---
+++ src/main/java/org/example/buskmate/messenger/Controller/ChatRoomController.java
@@ -0,0 +1,15 @@
+package org.example.buskmate.messenger.Controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/chat/rooms")
+public class ChatRoomController {
+
+
+
+}
