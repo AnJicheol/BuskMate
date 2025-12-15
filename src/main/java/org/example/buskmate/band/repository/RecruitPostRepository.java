6aff1fab6cbbb01f17cce9f16115d81717c0df2c
estherkim0109 <dptmej0501@naver.com>
2025-12-15 15:56:14 +0900

    refactor : repository 수정

--- src/main/java/org/example/buskmate/band/repository/RecruitPostRepository.java
+++ src/main/java/org/example/buskmate/band/repository/RecruitPostRepository.java
@@ -5,6 +5,8 @@ import org.example.buskmate.band.domain.RecruitPostStatus;
 import org.example.buskmate.band.dto.recruitpost.RecruitPostDetailResponseDto;
 import org.example.buskmate.band.dto.recruitpost.RecruitPostListDto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 import java.util.List;
@@ -15,7 +17,32 @@ public interface RecruitPostRepository extends JpaRepository<RecruitPost, Long> 
 
     List<RecruitPost> findAllByBandId(String bandId);
 
-    Optional<RecruitPostDetailResponseDto> findDetail(String postId);
+    @Query("""
+        select new org.example.buskmate.band.dto.recruitpost.RecruitPostDetailResponseDto(
+            rp.postId,
+            rp.band.bandId,
+            rp.title,
+            rp.content,
+            rp.status,
+            rp.createdAt
+        )
+        from RecruitPost rp
+        where rp.postId = :postId
+    """)
+    Optional<RecruitPostDetailResponseDto> findDetail(@Param("postId") String postId);
 
-    List<RecruitPostListDto> findAllByStatus(RecruitPostStatus status);
+    @Query("""
+      select new org.example.buskmate.band.dto.recruitpost.RecruitPostListDto(
+        rp.postId,
+        rp.band.bandId,
+        rp.title,
+        rp.status,
+        rp.createdAt
+      )
+      from RecruitPost rp
+      where rp.status = :status
+      order by rp.createdAt desc
+""")
+    List<RecruitPostListDto> findAllByStatus(@Param("status") RecruitPostStatus status);
+
 }
