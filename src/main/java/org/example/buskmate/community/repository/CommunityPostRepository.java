a8f7cb7d86a65e816d6337d3ed582f9a4b94da5c
1ShinSuHo1 <sshsh1004@naver.com>
2025-12-16 10:03:26 +0900

    fix: 쿼리 문제 해결

--- src/main/java/org/example/buskmate/community/repository/CommunityPostRepository.java
+++ src/main/java/org/example/buskmate/community/repository/CommunityPostRepository.java
@@ -13,27 +13,27 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface CommunityPostRepository extends JpaRepository<CommunityPost,Long> {
     @Query(
-        value = """
-            select new org.example.buskmate.community.dto.post.crud.response.CommunityPostReadAllPostResponse(
-                p.id,
-                p.authorId,
-                p.title,
-                cast(coalesce(count(distinct l.viewerId), 0L) as Long) as viewCount,
-                p.createdAt,
-                cast(coalesce(count(distinct c.id), 0L) as Long) as commentCount
-                )
-            from CommunityPost p
-            left join CommunityPostLog l on l.communityPost = p
-            left join CommunityComment c on c.communityPost = p and c.isActive = :status
-            where p.isActive = :status
-            group by p.id, p.authorId, p.title, p.createdAt
-            order by p.createdAt DESC
-        """,
-        countQuery = """
-            select count(p)
-            from CommunityPost p
-            where p.isActive = :status
-        """
+            value = """
+        select new org.example.buskmate.community.dto.post.crud.response.CommunityPostReadAllPostResponse(
+            p.id,
+            p.authorId,
+            p.title,
+            count(distinct l.viewerId),
+            p.createdAt,
+            count(distinct c.id)
+        )
+        from CommunityPost p
+        left join CommunityPostLog l on l.communityPost = p
+        left join CommunityComment c on c.communityPost = p and c.isActive = :status
+        where p.isActive = :status
+        group by p.id, p.authorId, p.title, p.createdAt
+        order by p.createdAt DESC
+    """,
+            countQuery = """
+        select count(p)
+        from CommunityPost p
+        where p.isActive = :status
+    """
     )
     Page<CommunityPostReadAllPostResponse> findAllPostSummary(
             @Param("status") PostStatus status,
